8b54a317ce4fa4d79057c66c98f2226abac2b1cd
Laurentiu <lcostin40@gmail.com>
2022-01-11 16:46:12 +0100

    Aggiunto controller e app.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div>
+        <h1>
+            Hello
+        </h1>
+
+        <section>
+            <h3>
+                Lista film
+            </h3>
+        </section>
+    </div>
+
+@endsection
