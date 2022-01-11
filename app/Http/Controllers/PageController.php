b48f57c6bcbd37b7e904a508121772720a426ff1
Laurentiu <lcostin40@gmail.com>
2022-01-11 16:48:39 +0100

    Cambiata route

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class PageController extends Controller
 {
     //
+
+    public function index()
+    {
+
+        return view('home');
+
+    }
 }
